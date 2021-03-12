6c3640141533e7ccd5ceacdb72d6505cb4de5f03
Francesco <80489428+sato-kazuma@users.noreply.github.com>
2021-03-12 15:45:59 +0100

    Update and rename level_order to level_order.c

--- Trees/level_order.c
+++ Trees/level_order.c
@@ -47,3 +47,20 @@ void LevelOrder(const Node* t) {
 		LevelOrderRec(t, 0, j);
 	}
 }
+
+//main
+#include"tree_int.h"
+extern void LevelOrder(const Node* t);
+int main(void) {
+	ElemType v1[] = { 1,2,3,4,5,6,7,8,9 };
+	Node* t1 = CreateRootTree(&v1[0],
+		CreateRootTree(&v1[1],
+			CreateRootTree(&v1[3],
+				CreateRootTree(&v1[6], CreateEmptyTree(), CreateEmptyTree()),
+				CreateRootTree(&v1[7], CreateEmptyTree(), CreateEmptyTree())),
+			CreateRootTree(&v1[4], CreateEmptyTree(), CreateEmptyTree())),
+		CreateRootTree(&v1[2], CreateRootTree(&v1[5], CreateEmptyTree(), CreateEmptyTree()), CreateEmptyTree()));
+	LevelOrder(t1);
+	return;
+
+}
