5bf8189758cd112a416caaedcbf9f67d64186dce
Francesco <80489428+sato-kazuma@users.noreply.github.com>
2021-03-12 15:55:55 +0100

    Create palindome.c

---
+++ Recorsive/palindome.c
@@ -0,0 +1,38 @@
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
+void conta(const char* str,int *cnt) {
+	while (*str != '\0') {
+		(*cnt)++;
+		*str++;
+	}
+	return;
+}
+void rec( const char* str, const char* reverse,int i,int *p) {
+	if (str[i] == '\0') {
+		return;
+	}
+	if (str[i] != reverse[i]) {
+		*p = false;
+		return;
+	}
+	else{
+	rec(str, reverse, i+1 ,p);
+	}
+}
+
+bool IsPalindromo(const char* str) {
+	if (str == NULL) { return false; }
+	int cnt = 0; int o = 0; int p = 1;
+	conta(str,&cnt);
+	if (cnt == 0) { return true; }
+	char* reverse = malloc(cnt+1 * sizeof(char));
+	for (int u = cnt-1; u>=0; u--) {
+		reverse[o] = str[u];
+		o++;
+	}
+	reverse[o] = '\0';
+	rec(reverse, str,0,&p);
+	free(reverse);
+	return p;
+}
