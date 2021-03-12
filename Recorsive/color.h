4cb020b4ade6ea31b5c06379789384a0e32850e8
Francesco <80489428+sato-kazuma@users.noreply.github.com>
2021-03-12 15:49:31 +0100

    Create color.h

---
+++ Recorsive/color.h
@@ -0,0 +1,12 @@
+#ifndef COLORA_H
+#define COLORA_H
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+struct Matrix {
+    size_t size;
+    bool* data;
+};
+extern int MappaColori(const struct Matrix* m, const char* c, size_t c_size);
+#endif // !COLORA_H
+
